dac5fc2c5b045377abf5f2fd77bd59e0e6917d78
AriGabay <arigabay19988@gmail.com>
2021-08-15 17:22:32 +0300

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
-  base: '/arigabay.github.io/',
+  base: '/arigabay.github.io/portfolio',
   plugins: [vue()]
 });
